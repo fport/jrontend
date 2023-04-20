dd26903c8b090f5e4f91b867bb858bbb91400781
fport <furkanportakalx@gmail.com>
2023-04-20 21:55:04 +0300

    core: add jrontends package ui

--- apps/app/src/pages/_app.tsx
+++ apps/app/src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
+import { Cabin } from 'next/font/google'
+
+const inter = Cabin({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <main className={inter.className}>
+            <Component {...pageProps} />
+        </main>
+    )
 }
